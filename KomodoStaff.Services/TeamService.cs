7c50122039047733cf72934cd4e56740f48af5af
tonyr <tonyr@knowledgeservices.com>
2018-10-22 11:13:29 -0400

    update code to add team

--- KomodoStaff.Services/TeamService.cs
+++ KomodoStaff.Services/TeamService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using KoimodoStaff.Data;
+using KomodoStaff.Data;
+using KomodoStaff.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +16,20 @@ namespace KomodoStaff.Services
         public TeamService(Guid userID)
         {
             _userId = userID;
+        }
+
+        public bool CreateTeam(TeamCreate model)
+        {
+            var entity =
+                 new Team()
+                 {
+                     Name = model.Name
+                 };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Teams.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
         }
     }
 }
